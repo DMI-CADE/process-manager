774cc454d142520989dc1ee05f3e6bf68b8df27d
Ben Krueger <Toffelpuffer1@gmx.de>
2020-11-22 19:24:28 +0100

    init(server; client): import from old rep

---
+++ tests/server.py
@@ -0,0 +1,32 @@
+import socket
+import os, os.path
+
+serversocket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+
+socketPath = '/tmp/dmicade_socket.s'
+
+if os.path.exists(socketPath):
+    os.remove(socketPath)
+
+print('Bind:', socketPath)
+serversocket.bind(socketPath)
+
+print('Listening for connection...')
+serversocket.listen(1)
+
+while True:
+    clientsocket, addr = serversocket.accept()
+
+    print('Connected: %s, %s' % (str(clientsocket), str(addr)))
+
+    msg = 'Message from Python Script...'
+    while msg != 'exit':
+        print(msg != 'exit')
+        msg = input("Send: ")
+        clientsocket.send(msg.encode('ascii'))
+
+    # recMsg = clientsocket.recv(1024)
+    # print('[Received Message]', recMsg.decode('ascii'))
+
+    clientsocket.close()
+    break
