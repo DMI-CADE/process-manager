e8d1f7ccb37ed224eeb58fbeb913f9fe96080013
Ben Krueger <Toffelpuffer1@gmx.de>
2021-04-02 01:53:57 +0200

    feat(uds server): it works now :)

--- dmicade_pm/core.py
+++ dmicade_pm/core.py
@@ -1,3 +1,4 @@
+from helper import dmicEvent
 import uds_server, window_handler
 
 class ProjectManager:
@@ -8,6 +9,23 @@ class ProjectManager:
     def start(self):
         socket_path = '/tmp/dmicade_socket.s'
         server = uds_server.UdsServer(socket_path)
+
+        server.connected_event += lambda x: print('Connected!')
+        server.received_event += lambda msg: print('Received: ', msg)
+
+        server.start()
+
+        while not server.is_connected():
+            pass
+
+        # Test
+        msg = ''
+        while msg != 'exit' and server.is_connected():
+            print('Send: ')
+            msg = input()
+            server.send(msg)
+
+        server.close()
 
         #win_handler = window_handler.WindowHandler()
         #win_handler.start()
--- dmicade_pm/uds_server.py
+++ dmicade_pm/uds_server.py
@@ -1,74 +1,87 @@
-import socket
+from helper import dmicEvent
+import socket, threading
 import os, os.path
 
 class UdsServer:
     """Unix Domain Socket Server for dmicade process manager."""
 
-    _server_socket = None
-    _socket_path = ""
-    _client_conn = None
+    CLIENT_TIMEOUT = 0.5
 
     def __init__(self, socket_path):
+        self.connected_event = dmicEvent()
+        self.received_event = dmicEvent()
+        self.disconnected_event = dmicEvent()
+
+        self._connected = False
+
         self._socket_path = socket_path
+        self._server_socket = None
+        self._client_conn = None
 
-    def start(self):
+        self._receive_thread = None
+        self._connect_thread = None
+
         self._server_socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-
         if os.path.exists(self._socket_path):
             os.remove(self._socket_path)
-
         self._server_socket.bind(self._socket_path)
 
-        self._server_socket.listen(1)
-        self._client_conn, addr = self._server_socket.accept()
+        # Start receive when connected.
+        self._receive_thread = threading.Thread(target=self._receive_continuous)
+        self.connected_event += lambda arg: self._receive_thread.start()
 
-    def send(self, message):
-        bytes_sent = self._client_conn.send(message.encode('ascii'))
-        print('bytesSent:', bytes_sent)
+    def start(self):
+        self._connect_thread = threading.Thread(target=self._connect)
+        self._connect_thread.start()
 
-    def disconnect(self):
-        pass
+    def close(self):
+        if self.is_connected():
+            try:
+                self._client_conn.shutdown(socket.SHUT_WR)
+                self._client_conn.close()
+            except:
+                #print('[UDS SERVER] Shutdown failed.')
+                pass
+
+            self._connected = False
 
     def _connect(self):
-        pass
+        #print('[UDS SERVER] Waiting for connection...')
+        self._server_socket.listen(1)
+        self._client_conn, addr = self._server_socket.accept()
+        self._client_conn.settimeout(self.CLIENT_TIMEOUT)
 
-    def _receive_msg(self):
-        print('Receiving Msg...')
-        rec_msg = self._client_conn.recv(1024)
-        print('[Received Message]', rec_msg.decode('ascii'))
+        self._connected = True
+        self.connected_event.update()
 
-    def _is_connected(self):
-        return self._client_conn != None
+    def send(self, message):
+        if self.is_connected():
+            bytes_sent = self._client_conn.send(message.encode('ascii'))
+            #print('[UDS SERVER] bytesSent:', bytes_sent)
 
+    def _receive_continuous(self):
+        #print('[UDS SERVER] Start continuous receive...')
+        while self.is_connected():
+            #print('[UDS SERVER] Receive...')
+            try:
+                rec_msg = self._client_conn.recv(1024)
+                msg = rec_msg.decode('ascii')
+                if len(msg) == 0:
+                    print('[UDS SERVER] Connection closed by remote host.')
+                    self.close()
+                    break
 
-#serversocket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-#
-#socketPath = '/tmp/dmicade_socket.s'
-#
-#if os.path.exists(socketPath):
-#    os.remove(socketPath)
-#
-#print('Bind:', socketPath)
-#serversocket.bind(socketPath)
-#
-#print('Listening for connection...')
-#serversocket.listen(1)
-#
-#while True:
-#    clientsocket, addr = serversocket.accept()
-#
-#    print('Connected: ', str(addr), str(clientsocket))
-#
-#    print('Receiving Msg...')
-#    recMsg = clientsocket.recv(1024)
-#    print('[Received Message]', recMsg.decode('ascii'))
-#
-#    msg = ''
-#    while msg != 'exit':
-#        print(msg != 'exit')
-#        msg = input("Send: ")
-#        bytesSent = clientsocket.send(msg.encode('ascii'))
-#        print("bytesSent:", bytesSent)
-#
-#    clientsocket.close()
-#    break
+                print('[UDS SERVER] Received:', msg)
+                self.received_event.update(msg)
+            except socket.timeout:
+                #print('[UDS SERVER] receive timeout...')
+                continue
+            except socket.error as e:
+                print('[UDS SERVER] receive exception raised:')
+                print(e)
+                self.close()
+                
+        #print('[UDS SERVER] Continuous receive stopped.')
+
+    def is_connected(self):
+        return self._connected
