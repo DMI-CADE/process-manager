82947a156704b63c8d75c63a4e394ba5eeaac462
Ben Krueger <Toffelpuffer1@gmx.de>
2021-04-07 18:23:03 +0200

    feat(client): add statem test + debug input thread

--- dmicade_pm/__main__.py
+++ dmicade_pm/__main__.py
@@ -1,5 +1,6 @@
+from .statemachine import DmicStateMachine, DmicTaskType
 from .uds_server import UdsServer
-from .statemachine import DmicStateMachine
+import threading
 
 
 def main():
@@ -18,7 +19,27 @@ class Client:
     def start(self):
         print('[PM CLIENT] Start')
 
-        input()
+        t = threading.Thread(target=self._debug_input)
+        t.start()
 
+        self._state_machine.queue_task_for_state({'type': DmicTaskType.TEST, 'data': ':)'})
+        self._state_machine.run_event_loop_sync()
+
+    def _debug_input(self):
+        user_input = ''
+        states = ['start', 'test']
+        state = 0
+
+        check_input = lambda x: user_input[:len(x)] == x
+
+        while not check_input('exit'):
+            user_input = input()
+            if check_input('test'):
+                self._state_machine.queue_task_for_state({'type': DmicTaskType.TEST, 'data': user_input})
+            elif check_input('swst'):
+                state = (state + 1) % len(states)
+                self._state_machine.queue_task_for_state({'type': DmicTaskType.CHANGE_STATE, 'data': states[state]})
+
+        self._state_machine.stop_event_loop()
 
 main()
