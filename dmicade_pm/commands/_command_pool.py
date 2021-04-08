ea38136642ab071ffc9329927abe399551523776
Ben Krueger <Toffelpuffer1@gmx.de>
2021-04-08 13:55:57 +0200

    feat(commands): add command pool

--- dmicade_pm/commands/_command_pool.py
+++ dmicade_pm/commands/_command_pool.py
@@ -1,13 +1,17 @@
 from .concrete_commands import *
+from ..helper import ObjectPool
 
 
-
-class CommandPool:
-    """Command Pool for concrete dmic commands"""
+class CommandPool(ObjectPool):
+    """Command Pool for concrete dmic commands."""
     COMMAND_PREFIX = 'C_'
 
-    def __init__(self):
-        self._pool = dict()
+    def __init__(self, process_manager):
+        super().__init__()
+        self.fill_object_pool(globals(), DmicCommand, self.COMMAND_PREFIX, process_manager, self)
+        print('[STATEM] Object pool:', self._pool)
 
-        for key in globals():
-            print(key)
+    def invoke_command(self, command_id, data):
+        if not self.object_exists(command_id):
+            raise Exception(f'[DMIC COMMAND POOL] Tried to invoke non existing command: {command_id}')
+        self._pool[command_id].execute(data)
