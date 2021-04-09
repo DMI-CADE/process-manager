7030895980a443592455bce4ecb177ba2f2a66d2
Ben Krueger <Toffelpuffer1@gmx.de>
2021-04-09 17:19:20 +0200

    feat(pm): add method signatures

--- dmicade_pm/processmanager.py
+++ dmicade_pm/processmanager.py
@@ -4,8 +4,32 @@ from .statemachine import DmicTask
 class DmicProcessManager:
     """Facade for controlling process manager components."""
 
-    def __init__(self, client):
+    def __init__(self, client, uds_server, timer):
         self._client = client
+        self._window_handler = None
+        self._uds_server = uds_server
+        self._timer = timer
+
+    def send_to_ui(self, msg: str):
+        pass
 
     def queue_state_task(self, task: DmicTask):
-        self._client.queue_state_task(task)
+        pass
+
+    def start_app(self, app_id):
+        pass
+
+    def focus_app(self, app_id):
+        pass
+
+    def close_app(self, app_id):
+        pass
+
+    def set_timer(self, time):
+        pass
+
+    def get_elapsed_time(self):
+        pass
+
+    def stop_timer(self):
+        pass
