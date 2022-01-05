58d3b5e199eb9f7827087f9720f7027936c7219f
Toffelpuffer <Toffelpuffer1@gmx.de>
2022-01-05 14:48:14 +0100

    feat(helper): add color codes for colored console logging

--- dmicade_pm/helper.py
+++ dmicade_pm/helper.py
@@ -27,6 +27,20 @@ def parse_command_line_arguments(cl_args):
     return user_args
 
 
+class ccolors:
+    """Values for colored console logging."""
+
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
 class DmicException(Exception):
     """Base class for exceptions in this module."""
     pass
