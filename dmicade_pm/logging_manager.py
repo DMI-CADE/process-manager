c1ffd7f00e7aeed94ef7a0309969cd3ccf816419
Toffelpuffer <43704691+Toffelpuffer@users.noreply.github.com>
2022-05-02 18:42:03 +0200

    feat(logging): add delete old logs functionality

--- dmicade_pm/logging_manager.py
+++ dmicade_pm/logging_manager.py
@@ -9,10 +9,15 @@ from .helper import ccolors
 
 class DmicLogging():
 
-    FILE_NAME_FORMAT = "DMIC-PM-%s.log"
+    FILE_NAME_FORMAT = 'DMIC-PM-%s.log'
+    DATE_FORMAT = r'%Y%m%d-%H%M%S'
+    DATE_REGEX = r'\d{8}-\d{6}' # Used to pull datetime out of FILE_NAME_FORMAT
+    LOG_LIFETIME_DAYS = 14
+    MINIMUM_EXISTING_LOGS = 5 # Starts deleting when more logs exist.
     FORMAT = '%(asctime)s - (%(threadName)-12.12s) %(levelname)-7.7s: %(filename)-25s: %(message)s'
 
     def __init__(self, user_args):
+        self.current_log_file = '' 
         self._no_file_log = 'nofilelog' in user_args
 
         self.log_folder = './logs'
@@ -55,8 +60,10 @@ class DmicLogging():
         if self._no_file_log:
             return
 
-        current_datetime_suffix = datetime.datetime.now().strftime('%Y%m%d-%H%M%S')
-        log_file_path = os.path.join(self.log_folder, self.FILE_NAME_FORMAT % current_datetime_suffix)
+        current_datetime_suffix = datetime.datetime.now().strftime(self.DATE_FORMAT)
+        self.current_log_file = self.FILE_NAME_FORMAT % current_datetime_suffix
+
+        log_file_path = os.path.join(self.log_folder, self.current_log_file)
         logging.info(f'[LOGGING MANAGER]: log to: {log_file_path}')
 
         file_handler = logging.FileHandler(log_file_path)
@@ -64,6 +71,25 @@ class DmicLogging():
         file_handler.setLevel(logging.INFO)
         root_logger.addHandler(file_handler)
 
+    def delete_old_logs(self):
+        existing_logs_amount = len(list(filter(lambda x: x[:4] == self.FILE_NAME_FORMAT[:4], os.listdir(self.log_folder))))
+        if existing_logs_amount < self.MINIMUM_EXISTING_LOGS:
+            return
+
+        delete_threshold = datetime.datetime.today() - datetime.timedelta(days=self.LOG_LIFETIME_DAYS)
+        for file_name in os.listdir(self.log_folder):
+
+            if file_name == self.current_log_file:
+                continue
+
+            log_datetime_str = re.search(self.DATE_REGEX, file_name).group()
+            log_datetime = datetime.datetime.strptime(log_datetime_str, self.DATE_FORMAT)
+            print(delete_threshold, log_datetime, delete_threshold > log_datetime)
+            if delete_threshold > log_datetime:
+                log_path = os.path.join(self.log_folder, file_name)
+                os.remove(log_path)
+                logging.info(f'[LOGGING MANAGER]: Deleted old log: {log_path}')
+            
 
 class DmicConsoleFormatter(logging.Formatter):
 
